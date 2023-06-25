390d1212c748f0771239fe4585c836e6de9a52b5
Adelynnz <adelynntanyijun@gmail.com>
2023-06-26 06:53:27 +0800

    Updated settings window
    Added settings window, resolution dropdown, fullscreen toggle, color background options, tile size and font size. Color background options, tile size and font size unable to work yet.
    
    Also adjusted the change scene from lobby to ingame.

--- Mahjong Intern/Assets/Scripts/SettingData.cs
+++ Mahjong Intern/Assets/Scripts/SettingData.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingData : MonoBehaviour
 {
@@ -9,4 +11,55 @@ public class SettingData : MonoBehaviour
         //Carry object to other scenes
         DontDestroyOnLoad(this.gameObject);
     }
+
+    //public Image changeBackgroundcolor;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    //Start is called before first frame update
+    void start()
+    {
+        //Setting variables
+        resolutions = Screen.resolutions;
+
+        //Clear resolution dropdown options
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    //Set resolution
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    //Toggle Fullscreen
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;   //Set fullscreen
+    }
+
+    //public void changeNavyBlue()
+    //{
+    //    changeBackgroundcolor.color = new Color32(21, 34 ,56,255);
+    //}
 }
