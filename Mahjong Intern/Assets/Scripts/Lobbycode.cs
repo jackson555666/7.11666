1897f65b720db41f2e9cadb9442b5bdcef02209f
Maydic489 <74528891+Maydic489@users.noreply.github.com>
2023-06-26 20:11:43 +0700

    Fixed not show random code bug

--- Mahjong Intern/Assets/Scripts/Lobbycode.cs
+++ Mahjong Intern/Assets/Scripts/Lobbycode.cs
@@ -20,18 +20,25 @@ public class Lobbycode : MonoBehaviour
 
     }
 
-    public void codecheck()
+    //this run when transition to next scene
+    public void Codecheck()
     {
         GameObject codetext = GameObject.Find("TextRoomCode");
-        Debug.Log(codetext != null);
-        if (codetext != null)
+
+        //check if found the TextRoomCode object and check if we input any code from Start scene
+        if (codetext != null && !string.IsNullOrEmpty(codeInputField.text))
         {
             codetext.GetComponent<TextMeshProUGUI>().text = "Room code: " + inputdata;
         }
     }
-    public void getCodeFromInputField()
+    public void GetCodeFromInputField()//this run when click start button
     {
-        if (codeInputField != null)
+        //this old code check if inputField gameobject exist instead of the text in inputField
+        //if (codeInputField != null)
+        //    inputdata = codeInputField.text;
+
+        //when check if string or text is empty, try use IsNullOrEmpty instead
+        if (!string.IsNullOrEmpty(codeInputField.text))
             inputdata = codeInputField.text;
     }
 
