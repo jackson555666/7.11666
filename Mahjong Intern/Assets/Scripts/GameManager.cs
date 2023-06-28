2d0de626bced043e23890488737cce3e93e48647
Adelynnz <adelynntanyijun@gmail.com>
2023-06-28 21:28:25 +0800

    Added Timer function to GameManager

--- Mahjong Intern/Assets/Scripts/GameManager.cs
+++ Mahjong Intern/Assets/Scripts/GameManager.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     TileGameObject selectedTile;
     public GameObject discardPile;
 
-    public List<TileGameObject> discardTileList = new List<TileGameObject>();
+    public List<TileGameObject> discardTileList = new List<TileGameObject>();   
+
+    public float timeLeft = 30.0f;  //assign float "timeLeft" as 30.0
+    public TMP_Text TimerText;  //used to show for countdown
+    public bool TimerOn = false;    //assign boolean "TimerOn" as false
+
 
     //for create Singleton pattern, only one of this script instance can exist in a scene
     public static GameManager Instance;
-
+    
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -26,7 +34,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        
+        TimerOn = true;     //On Timer countdown
     }
 
     public void DiscardATile()
@@ -60,5 +68,23 @@ public class GameManager : MonoBehaviour
 
         selectedTile.MoveTileDown();
         selectedTile = null;
+    }
+
+    private void Update()
+    {
+        if (TimerOn == true)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                TimerText.SetText("Timer: " + timeLeft.ToString("0"));
+            }
+            else
+            {
+                timeLeft = 0;
+                TimerText.SetText("Timer: " + timeLeft.ToString("0"));
+                //Do something after time countdown
+            }
+        }
     }
 }
